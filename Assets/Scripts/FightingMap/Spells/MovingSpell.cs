898bbc9b7bc6a0fb01906be77ebedc2931e5b341
GuillaumeDuc <ducoeurguillaume@gmail.com>
2021-07-01 00:31:47 +0200

    wip delay destroy moving spell

--- Assets/Scripts/FightingMap/Spells/MovingSpell.cs
+++ Assets/Scripts/FightingMap/Spells/MovingSpell.cs
@@ -9,12 +9,21 @@ public class MovingSpell : MonoBehaviour
 
     public float beforeImpactX = 0, beforeImpactY = 0;
 
+    public float destroyDelay = 0;
+
     private Transform rb;
     private Vector3 target;
+    private bool reached = false;
     void Start()
     {
         rb = transform.GetComponent<Transform>();
         target = new Vector3(transform.parent.position.x + (float)beforeImpactX, transform.parent.position.y + (float)beforeImpactY, 0);
+    }
+
+    private IEnumerator delayDestroy()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(transform.parent.gameObject);
     }
 
     void FixedUpdate()
@@ -24,10 +33,11 @@ public class MovingSpell : MonoBehaviour
         {
             rb.position = Vector3.MoveTowards(rb.position, target, Time.deltaTime * speed);
         }
-        else if (rb != null && nextGO != null)
+        else if (rb != null && nextGO != null && reached == false)
         {
             Instantiate(nextGO, target, Quaternion.identity);
-            Destroy(transform.parent.gameObject);
+            StartCoroutine(delayDestroy());
+            reached = true;
         }
     }
 }
