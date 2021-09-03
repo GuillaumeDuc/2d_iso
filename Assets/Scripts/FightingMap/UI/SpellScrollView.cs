d0c2c9bb72f562cc3d795f958359988093b7b6b2
GuillaumeDuc <ducoeurguillaume@gmail.com>
2021-09-03 16:52:37 +0200

    check for null in spell scroll view

--- Assets/Scripts/FightingMap/UI/SpellScrollView.cs
+++ Assets/Scripts/FightingMap/UI/SpellScrollView.cs
@@ -12,17 +12,20 @@ public class SpellScrollView : MonoBehaviour
 
     public void addSpell(GameObject spell, Unit unit)
     {
-        // Instantiate button
-        GameObject newSpellButton = Instantiate(SpellButtonPrefab);
-        // Get GameObject and set sprite
-        GameObject spellPicture = newSpellButton.transform.GetChild(0).gameObject;
-        Image ImageSP = spellPicture.GetComponent<Image>();
-        ImageSP.sprite = spell.GetComponent<SpriteRenderer>().sprite;
-        // Add onClick Effect
-        Button buttonComponent = newSpellButton.GetComponent<Button>();
-        buttonComponent.onClick.AddListener(() => { onClickSpell(spell, unit); });
-        // Put button in scrollviewer
-        newSpellButton.transform.SetParent(contentContainer.transform, false);
+        if (spell != null)
+        {
+            // Instantiate button
+            GameObject newSpellButton = Instantiate(SpellButtonPrefab);
+            // Get GameObject and set sprite
+            GameObject spellPicture = newSpellButton.transform.GetChild(0).gameObject;
+            Image ImageSP = spellPicture.GetComponent<Image>();
+            ImageSP.sprite = spell.GetComponent<SpriteRenderer>().sprite;
+            // Add onClick Effect
+            Button buttonComponent = newSpellButton.GetComponent<Button>();
+            buttonComponent.onClick.AddListener(() => { onClickSpell(spell, unit); });
+            // Put button in scrollviewer
+            newSpellButton.transform.SetParent(contentContainer.transform, false);
+        }
     }
 
     void onClickSpell(GameObject spell, Unit unit)
